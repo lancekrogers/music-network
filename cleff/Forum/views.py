4d8dc40ca194034e9de491d0551473721258c6f3
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-29 00:00:30 -0400

    upvote down vote view created and fully functional

--- cleff/Forum/views.py
+++ cleff/Forum/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import random
+from django.shortcuts import render, redirect
 from profiles.models import Musician, NonMusician
 from .models import Vote, MusicianPost, MusicianResponse
 # Create your views here.
@@ -9,46 +10,39 @@ def vote_create(request, votee_pk, model_type, vote_type='upvote'):
     if request.POST:
         print('sent post')
         user_pk = request.user.pk
-        musician_post = False
-        answer = False
-        downvote = False
-        upvote = True
         profile = Musician.objects.get(pk=user_pk)
-        model_type = model_type
-        obj = ''
-        if model_type == 'musician_post':
-            musician_post = True
-            obj = MusicianPost.objects.get(pk=votee_pk)
-        if model_type == 'musician_response':
-            answer = True
-            obj = MusicianResponse.objects.get(pk=votee_pk)
-            x_var = obj.question.pk
-        if vote_type == 'downvote':
-            downvote = True
-            upvote = False
-            obj.score -= 5
-            obj.save()
+        if not Vote.objects.filter(votee_pk=votee_pk).filter(voter=profile):
+            musician_post = False
+            answer = False
+            downvote = False
+            upvote = True
+            model_type = model_type
+            obj = ''
+            if model_type == 'musician_post':
+                musician_post = True
+                obj = MusicianPost.objects.get(pk=votee_pk)
+            if model_type == 'musician_response':
+                answer = True
+                obj = MusicianResponse.objects.get(pk=votee_pk)
+                x_var = obj.question.pk
+            if vote_type == 'downvote':
+                downvote = True
+                upvote = False
+                obj.score -= 5
+                obj.save()
+            else:
+                obj.score += 10
+                obj.save()
+            vote = Vote.objects.create(votee_pk=votee_pk,
+                                       voter=profile,
+                                       upvote=upvote,
+                                       downvote=downvote,
+                                       completed=True,
+                                       is_post=musician_post,
+                                       is_response=answer)
+            vote.save()
         else:
-            numset = '000000001'
-            if profile.score < 25:
-                numset = '010021'
-            rand = random.choice(numset)
-            owner = Profile.objects.get(pk=obj.user.pk)
-            owner.score += 10
-            print('It works here is the owners score:', owner.score)
-            profile.score += int(rand)
-            profile.save()
-            obj.score += 10
-            obj.save()
-            owner.save()
-        vote = Vote.objects.create(votee_pk=votee_pk,
-                                   voter=profile,
-                                   upvote=upvote,
-                                   downvote=downvote,
-                                   completed=True,
-                                   is_question=question,
-                                   is_answer=answer)
-        vote.save()
-        return redirect('stack:question_page', question_id=x_var)
+            pass
+        return redirect('Forum:musician_post_page', musician_post_id=x_var)
     else:
-       return redirect('stack:question_page', question_id=x_var)
+       return redirect('Forum:musician_post_page', musician_post_id=x_var)
